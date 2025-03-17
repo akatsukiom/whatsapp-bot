0912ebd7f263518db418daa4dd1c241371ae6faf
akatsukiom <akatsukiomeg@gmail.com>
2025-03-17 15:06:04 -0600

    learning moduloe

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const socketIo = require('socket.io');
 const fs = require('fs');
 const path = require('path');
 const config = require('./modules/config');
-const utils = require('./utils');
+const utils = require('./modules/utils/utils');
 const dotenv = require('dotenv');
 
 function setupServer() {
