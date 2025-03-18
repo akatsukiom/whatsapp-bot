d0602c0975bfc27afcf6bc251eeb54385af1c312
akatsukiom <akatsukiomeg@gmail.com>
2025-03-17 21:25:05 -0600

    learning moduloe

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ try {
       learningData: path.resolve(__dirname, 'learning-data.json')
     },
     server: {
-      port: process.env.PORT || 8000,
+      port: process.env.PORT || 3000,
       host: process.env.HOST || 'localhost'
     },
     openai: {
