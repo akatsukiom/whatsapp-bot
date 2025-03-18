e87286e685d38b0c9d68e9cd4e554bfca676ff5b
akatsukiom <akatsukiomeg@gmail.com>
2025-03-17 20:23:23 -0600

    learning moduloe

--- routes/publicar.js
+++ routes/publicar.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { publishToAllGroups } = require('./publisher');
+const { publishToAllGroups } = require('../modules/publisher');
 
 // Esta ruta POST se usará para publicar en todos los grupos
 router.post('/publicar', async (req, res) => {
